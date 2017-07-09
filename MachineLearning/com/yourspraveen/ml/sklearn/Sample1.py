4d4ddc6f9f35672957c81ff280d636f1af957525
Praveen K Palaniswamy <praveen.palaniswamy@gmail.com>
2017-07-08 23:48:34 -0500

    update

--- MachineLearning/com/yourspraveen/ml/sklearn/Sample1.py
+++ MachineLearning/com/yourspraveen/ml/sklearn/Sample1.py
@@ -5,5 +5,36 @@ Created on Jul 8, 2017
 '''
 
 from sklearn import datasets
+
+#getting data set of Iris
 iris = datasets.load_iris()
-digits = datasets.load_digits()
+
+#features -> Input
+X = iris.data
+
+#expected results -> Output
+y = iris.target
+
+#generating Training and Test data sets as equal split of iris data
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
+
+#classifier algorithm -> Decision Tree
+#from sklearn.tree import DecisionTreeClassifier
+#classifier = DecisionTreeClassifier()
+
+#classifier algorithm
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier()
+
+#Training
+classifier.fit(X_train, y_train)
+
+#Test Data set for verification
+predictions = classifier.predict(X_test)
+
+#Result set of test data set
+#print(dTreePredictions)
+
+from sklearn.metrics import accuracy_score
+print accuracy_score(y_test, predictions)
